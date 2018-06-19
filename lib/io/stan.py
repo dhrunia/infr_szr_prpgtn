762053ca56dd16288fe39504c59ac76d2e889ac4
Anirudh Nihalani <anirudhnihalani@gmail.com>
2018-06-19 17:19:56 +0200

    updated read_samples method to read some sampler meta data as well

--- lib/io/stan.py
+++ lib/io/stan.py
@@ -33,8 +33,8 @@ def rdump(fname, data):
                 except:
                     pass
                 line = '%s <- %s' % (key, val)
-            fd.write(line)
-            fd.write('\n')
+                fd.write(line)
+                fd.write('\n')
 
 
 def rload(fname):
@@ -42,7 +42,7 @@ def rload(fname):
     """
     with open(fname, 'r') as fd:
         lines = fd.readlines()
-    data = {}
+        data = {}
     for line in lines:
         lhs, rhs = [_.strip() for _ in line.split('<-')]
         if rhs.startswith('structure'):
@@ -60,7 +60,7 @@ def rload(fname):
                     val = float(rhs)
                 except:
                     raise ValueError(rhs)
-        data[lhs] = val
+                data[lhs] = val
     return data
 
 
@@ -99,15 +99,15 @@ def parse_csv(fname, merge=True):
         for line in fd.readlines():
             if not line.startswith('#'):
                 lines.append(line.strip().split(','))
-    names = [field.split('.') for field in lines[0]]
-    data = np.array([[float(f) for f in line] for line in lines[1:]])
+                names = [field.split('.') for field in lines[0]]
+                data = np.array([[float(f) for f in line] for line in lines[1:]])
 
     namemap = {}
     maxdims = {}
     for i, name in enumerate(names):
         if name[0] not in namemap:
             namemap[name[0]] = []
-        namemap[name[0]].append(i)
+            namemap[name[0]].append(i)
         if len(name) > 1:
             maxdims[name[0]] = name[1:]
 
@@ -115,7 +115,7 @@ def parse_csv(fname, merge=True):
         dims = []
         for dim in maxdims[name]:
             dims.append(int(dim))
-        maxdims[name] = tuple(reversed(dims))
+            maxdims[name] = tuple(reversed(dims))
 
     # data in linear order per Stan, e.g. mat is col maj
     # TODO array is row maj, how to distinguish matrix v array[,]?
@@ -140,7 +140,7 @@ def parse_summary_csv(fname):
             _, k, v = line.split('"')
             skeys.append(k)
             svals.append(np.array([float(_) for _ in v.split(',')[1:]]))
-    svals = np.array(svals)
+            svals = np.array(svals)
 
     sdat = {}
     sdims = {}
@@ -151,7 +151,7 @@ def parse_summary_csv(fname):
             sdims[name] = dim
             if name not in sdat:
                 sdat[name] = []
-            sdat[name].append(sval)
+                sdat[name].append(sval)
         else:
             sdat[skey] = sval
 
@@ -173,7 +173,7 @@ def csv2mode(csv_fname, mode=None):
             val_ = val.mean(axis=0)
         elif mode[0] == 'p':
             val_ = np.percentile(val, int(mode[1:]), axis=0)
-        data[key] = val_
+            data[key] = val_
     return data
 
 
@@ -190,7 +190,7 @@ def cmdstan_path(path=''):
     if path:
         path = os.path.expanduser(os.path.expandvars(path))
         os.environ['CMDSTAN'] = path
-    path = os.environ.get('CMDSTAN', 'cmdstan')
+        path = os.environ.get('CMDSTAN', 'cmdstan')
     if not os.path.exists(os.path.join(path, 'runCmdStanTests.py')):
         raise CmdStanNotFound(
             'please provide CmdStan path, e.g. lib.cmdstan_path("/path/to/")')
@@ -210,7 +210,7 @@ def compile_model(stan_fname, cc='clang++'):
     stdout = proc.stdout.read().decode('ascii').strip()
     if stdout:
         print(stdout)
-    stderr = proc.stderr.read().decode('ascii').strip()
+        stderr = proc.stderr.read().decode('ascii').strip()
     if stderr:
         print(stderr)
 
@@ -221,12 +221,12 @@ def create_process(cmd,block=None, stdout=sys.stdout, stderr=sys.stderr):
         while subProc.poll() is None:
             stdout.write(subProc.stdout.read(1))
             stdout.flush()
-        stdout.write(subProc.stdout.read())
-        stdout.flush()
-        #  if(subProc.returncode):
-        #    stderr.write(subProc.stderr.read())
-        #    stderr.flush()
-        #    raise Exception("Error executing "+' '.join(cmd))
+            stdout.write(subProc.stdout.read())
+            stdout.flush()
+            #  if(subProc.returncode):
+            #    stderr.write(subProc.stderr.read())
+            #    stderr.flush()
+            #    raise Exception("Error executing "+' '.join(cmd))
         err = subProc.stderr.read()
         if(err):
             stderr.write(err)
@@ -253,30 +253,50 @@ def rem_warmup_samples(src_fname,trgt_fname,num_warmup_samples):
                     for i in range(num_warmup_samples):
                         fd1.readline()
                     break
-            t = fd1.readline()
+                t = fd1.readline()
             while(t):
                 fd2.write(t)
                 t = fd1.readline()
-    
-def read_samples(csv_fname,nwarmup,nsampling,ignore_warmup=False,variables_of_interest=[]):
-    if(ignore_warmup):
-        nsamples = nsampling
-    else:
-        nsamples = nwarmup + nsampling
-    with open(csv_fname,'r') as fd:
-        t = fd.readline()
+
+
+def read_one_sample(line, data, sample_idx, var_names, var_dims, var_start_idx):
+    var_vals = [float(el.strip()) for el in line.split(',')]
+    for var_name in var_names:
+        start_idx = var_start_idx[var_name]
+        end_idx = start_idx + (np.product(var_dims[var_name]) if(var_dims[var_name]) else 1)
+        data[var_name][sample_idx] = np.array(var_vals[start_idx:end_idx]).reshape(var_dims[var_name], order='F')
+
+
+def read_samples(csv_fname, nwarmup=0, nsampling=0, ignore_warmup=True, variables_of_interest=[]):
+    '''
+    Reads specified variables from cmdstan output csv file
+
+    csv_fname : cmdstan's output csv file name
+    nwarmup   : Number of warmup samples to read
+    nsampling : Number of samples after warmup
+    ignore_warmup : Don't read warmup samples
+    variables_of_interest : list of variable names to read
+    '''
+    nsamples = nwarmup + nsampling
+    with open(csv_fname, 'r') as fd:
+        t = fd.readline().strip()
         read_head = False
         sample_idx = 0
-        warmup_samples_read = 0
+        sample_count = 0
         while(t):
             if(t[0] == '#'):
-                pass
-            elif(not read_head): # Extract variable names and their dimensions from the heading of the csv
+                if("num_samples" in t):
+                    sampling_iters = int(t[1:].strip().split('=')[1].strip())
+                elif("num_warmup" in t):
+                    warmup_iters = int(t[1:].strip().split('=')[1].strip())
+                    if(nsamples == 0):
+                        nsamples = sampling_iters + 0 if(ignore_warmup) else warmup_iters
+            elif(not read_head):  # Extract variable names and their dimensions from the heading of the csv
                 var_names = []
                 var_dims = {}
                 var_start_idx = {}
                 col_names = t.split(',')
-                for i,name in enumerate(col_names):
+                for i, name in enumerate(col_names):
                     var_name = name.strip().split('.')[0]
                     var_dim = [int(dim) for dim in name.strip().split('.')[1:]]
                     if(var_name not in var_names):
@@ -285,21 +305,23 @@ def read_samples(csv_fname,nwarmup,nsampling,ignore_warmup=False,variables_of_in
                     var_dims[var_name] = var_dim
                 read_head = True
                 data = {}
+                var_names = variables_of_interest if(variables_of_interest) else var_names
                 # Create a dictionary (variable name -> numpy.ndarray) for storing data
-                for var_name in (variables_of_interest if(variables_of_interest) else var_names):
-                    data[var_name] = np.ndarray(shape = [nsamples] + var_dims[var_name],dtype=float)
+                for var_name in var_names:
+                    data[var_name] = np.ndarray(shape=[nsamples] + var_dims[var_name], dtype=float)
             else:
-                if(ignore_warmup and warmup_samples_read != nwarmup):
-                    warmup_samples_read += 1
+                if(ignore_warmup and sample_count < warmup_iters):
+                    pass
+                elif(not ignore_warmup and sample_count < nwarmup):
+                    read_one_sample(t, data, sample_idx, var_names, var_dims, var_start_idx)
+                    sample_idx += 1
+                elif(not ignore_warmup and sample_count < warmup_iters):
+                    pass
                 else:
-                    # print(f'{sample_idx}: {t[0:20]}') 
-                    var_vals = [float(el.strip()) for el in t.split(',')]
-                    for var_name in (variables_of_interest if(variables_of_interest) else var_names):
-                        start_idx = var_start_idx[var_name] 
-                        end_idx = start_idx + (np.product(var_dims[var_name]) if(var_dims[var_name]) else 1)
-                        data[var_name][sample_idx] = np.array(var_vals[start_idx:end_idx]).reshape(var_dims[var_name],order='F')
+                    read_one_sample(t, data, sample_idx, var_names, var_dims, var_start_idx)
                     sample_idx += 1
                     if(sample_idx == nsamples):
                         break
+                sample_count += 1
             t = fd.readline()
     return data
