e2761a68268890c3892599b84ca206163210942e
anirudh <anirudhnihalani@gmail.com>
2019-09-16 18:06:30 +0200

    formated with autopep8/yapf

--- lib/io/seeg.py
+++ lib/io/seeg.py
@@ -24,11 +24,14 @@ def load_eeg(fname, proj_bip=None):
     seeg = seeg[:-2]
     return _maybe_bip(seeg, time, proj_bip)
 
+
 def read_contacts(cntcts_file, type='dict'):
-    cntcts = zip(np.loadtxt(cntcts_file, usecols=[0], dtype='str'), np.loadtxt(cntcts_file, usecols=[1,2,3]))
-    if(type == 'dict'):
+    cntcts = zip(
+        np.loadtxt(cntcts_file, usecols=[0], dtype='str'),
+        np.loadtxt(cntcts_file, usecols=[1, 2, 3]))
+    if (type == 'dict'):
         return dict(cntcts)
-    elif(type == 'list'):
+    elif (type == 'list'):
         return list(cntcts)
 
 
@@ -41,8 +44,9 @@ def find_picks(json_fnames):
         if (meta_data['type'].lower() == "spontaneous seizure"):
             exclude = meta_data['bad_channels'] + meta_data['non_seeg_channels']
             ch_names = set(
-                mne.io.read_info(f'{os.path.dirname(fname)}/{fname_wo_xtnsn}.raw.fif')[
-                    'ch_names'])
+                mne.io.read_info(
+                    f'{os.path.dirname(fname)}/{fname_wo_xtnsn}.raw.fif')[
+                        'ch_names'])
             picks.append(ch_names - set(exclude))
         else:
             raise Exception('Not a spontaneous seizure')
@@ -64,11 +68,11 @@ def read_seeg(data_dir, meta_data_fname, raw_seeg_fname):
         assert meta_data['onset'] is not None and meta_data['termination'] is not None
         raw.pick_types(meg=False, eeg=True)
         raw.pick_channels(picks)
-        raw.crop(tmin=meta_data['onset']-5, tmax=meta_data['termination']+5)
+        raw.crop(
+            tmin=meta_data['onset'] - 5, tmax=meta_data['termination'] + 5)
         raw.reorder_channels(picks)
         seeg['fname'] = f'{fname_wo_xtnsn}'
         seeg['time_series'] = raw.get_data().T
         seeg['sfreq'] = raw.info['sfreq']
         seeg['ch_names'] = raw.ch_names
     return seeg
-
