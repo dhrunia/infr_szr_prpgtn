197f9eceb205fcd63aedf2737532106d64f14119
Anirudh <anirudhnihalani@gmail.com>
2019-10-24 18:50:52 +0200

    add function to find ez and pz of a signle optimzation run

--- retro_results.py
+++ retro_results.py
@@ -217,8 +217,23 @@ def find_ez(onst_thrshld, bin_thrshld, nbins, patient_ids, root_dir):
         ez_pred = np.zeros(nn)
         ez_pred[np.where(onsets<b[bin_thrshld])] = 1
         np.save(os.path.join(root_dir, patient_id, 'ez_pred.npy'), ez_pred)
-    
-    
+   
+
+def find_ez_single(onst_thrshld, bin_thrshld, nbins, csv_path, szr_len):
+    optima = lib.io.stan.read_samples([csv_path])
+    x = optima['x'][0]
+    nn = x.shape[1]
+    onsets = (szr_len + 50)*np.ones(nn)
+    for i in range(nn):
+        xt = x[:,i] > onst_thrshld
+        if(xt.any()):
+            onsets[i] = np.where(x[:, i] > onst_thrshld)[0][0]
+    a, b = np.histogram(onsets[onsets<szr_len], bins=nbins)
+    ez = np.nonzero(onsets < b[bin_thrshld])[0]
+    pz = np.nonzero(np.logical_and(onsets > b[bin_thrshld], onsets < szr_len))[0]
+    return ez, pz
+
+
 def precision_recall(patient_ids, root_dir):
     tp = fp = fn = 0
     for patient_id in patient_ids:
@@ -239,6 +254,7 @@ def precision_recall(patient_ids, root_dir):
     precision = tp / (tp + fp)
     recall = tp / (tp + fn)
     return precision, recall
+
 
 def tpr_and_fpr(patient_ids, root_dir):
     tp = fp = fn = tn = 0
@@ -264,132 +280,133 @@ def tpr_and_fpr(patient_ids, root_dir):
     return tpr, fpr
 
 
-root_dir = '/home/anirudh/Nextcloud/Academia/Projects/VEP/results/exp10.67'
-patient_ids = dict()
-patient_ids['engel1'] = ['id001_bt','id003_mg','id004_bj','id010_cmn','id013_lk','id014_vc','id017_mk','id020_lma','id022_te','id025_mc','id027_sj','id030_bf','id039_mra','id050_sx']
-patient_ids['engel2or3or4'] = ['id007_rd','id008_dmc','id021_jc','id023_br','id028_ca','id033_fc','id036_dm', 'id037_cg','id040_ms']
+if (__name__ == '__main__'):
+    root_dir = '/home/anirudh/Nextcloud/Academia/Projects/VEP/results/exp10.67'
+    patient_ids = dict()
+    patient_ids['engel1'] = ['id001_bt','id003_mg','id004_bj','id010_cmn','id013_lk','id014_vc','id017_mk','id020_lma','id022_te','id025_mc','id027_sj','id030_bf','id039_mra','id050_sx']
+    patient_ids['engel2or3or4'] = ['id007_rd','id008_dmc','id021_jc','id023_br','id028_ca','id033_fc','id036_dm', 'id037_cg','id040_ms']
 
-# Precision-Recall curves for bin thresholding
-precision = []
-recall = []
-onst_thrshld = -0.05
-nbins = 20
-bin_thrshld =  np.arange(1, nbins, dtype=int)
-for bin_thrshld_ in bin_thrshld:
-    find_ez(onst_thrshld, bin_thrshld_, nbins, patient_ids['engel1'], root_dir)
-    p, r = precision_recall(patient_ids['engel1'], root_dir)
-    precision.append(p)
-    recall.append(r)
-plt.figure()
-plt.plot(recall, precision, color='black')
-plt.xlabel('Recall', fontsize=13)
-plt.ylabel('Precision', fontsize=13)
-plt.xticks(fontsize=12)
-plt.yticks(fontsize=12)
-plt.title('Engel score I', fontsize=15)
-plt.show(block=False)
+    # Precision-Recall curves for bin thresholding
+    precision = []
+    recall = []
+    onst_thrshld = -0.05
+    nbins = 20
+    bin_thrshld =  np.arange(1, nbins, dtype=int)
+    for bin_thrshld_ in bin_thrshld:
+        find_ez(onst_thrshld, bin_thrshld_, nbins, patient_ids['engel1'], root_dir)
+        p, r = precision_recall(patient_ids['engel1'], root_dir)
+        precision.append(p)
+        recall.append(r)
+    plt.figure()
+    plt.plot(recall, precision, color='black')
+    plt.xlabel('Recall', fontsize=13)
+    plt.ylabel('Precision', fontsize=13)
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=12)
+    plt.title('Engel score I', fontsize=15)
+    plt.show(block=False)
 
-precision = []
-recall = []
-onst_thrshld = -0.05
-nbins = 20
-bin_thrshld = np.arange(1, nbins, dtype=int)
-for bin_thrshld_ in bin_thrshld:
-    find_ez(onst_thrshld, bin_thrshld_, nbins, patient_ids['engel2or3or4'], root_dir)
-    p, r = precision_recall(patient_ids['engel2or3or4'], root_dir)
-    precision.append(p)
-    recall.append(r)
-plt.figure()
-plt.plot(recall, precision, color='black')
-plt.xlabel('Recall', fontsize=13)
-plt.ylabel('Precision', fontsize=13)
-plt.xticks(fontsize=12)
-plt.yticks(fontsize=12)
-plt.title('Engel score II, III and IV', fontsize=15)
-plt.show(block=False)
+    precision = []
+    recall = []
+    onst_thrshld = -0.05
+    nbins = 20
+    bin_thrshld = np.arange(1, nbins, dtype=int)
+    for bin_thrshld_ in bin_thrshld:
+        find_ez(onst_thrshld, bin_thrshld_, nbins, patient_ids['engel2or3or4'], root_dir)
+        p, r = precision_recall(patient_ids['engel2or3or4'], root_dir)
+        precision.append(p)
+        recall.append(r)
+    plt.figure()
+    plt.plot(recall, precision, color='black')
+    plt.xlabel('Recall', fontsize=13)
+    plt.ylabel('Precision', fontsize=13)
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=12)
+    plt.title('Engel score II, III and IV', fontsize=15)
+    plt.show(block=False)
 
-# ## Precision-Recall curves for onset thresholding
-# precision = []
-# recall = []
-# nbins = 10
-# onst_thrshld = np.linspace(0, -2.0, 10)
-# bin_thrshld = 1
-# for onst_thrshld_ in onst_thrshld:
-#     find_ez(onst_thrshld_, bin_thrshld, nbins, patient_ids['engel1'], root_dir)
-#     p, r = precision_recall(patient_ids['engel1'], root_dir)
-#     precision.append(p)
-#     recall.append(r)
-#     print(onst_thrshld_, p, r)
-# plt.figure()
-# plt.plot(recall, precision)
-# plt.xlabel('Recall', fontsize=13)
-# plt.ylabel('Precision', fontsize=13)
-# plt.title('Engel score I', fontsize=15)
-# plt.show(block=False)
+    # ## Precision-Recall curves for onset thresholding
+    # precision = []
+    # recall = []
+    # nbins = 10
+    # onst_thrshld = np.linspace(0, -2.0, 10)
+    # bin_thrshld = 1
+    # for onst_thrshld_ in onst_thrshld:
+    #     find_ez(onst_thrshld_, bin_thrshld, nbins, patient_ids['engel1'], root_dir)
+    #     p, r = precision_recall(patient_ids['engel1'], root_dir)
+    #     precision.append(p)
+    #     recall.append(r)
+    #     print(onst_thrshld_, p, r)
+    # plt.figure()
+    # plt.plot(recall, precision)
+    # plt.xlabel('Recall', fontsize=13)
+    # plt.ylabel('Precision', fontsize=13)
+    # plt.title('Engel score I', fontsize=15)
+    # plt.show(block=False)
 
-# precision = []
-# recall = []
-# nbins = 10
-# onst_thrshld =  np.linspace(0, -0.5, 20)
-# bin_thrshld = 1
-# for onst_thrshld_ in onst_thrshld:
-#     find_ez(onst_thrshld_, bin_thrshld, nbins, patient_ids['engel2or3or4'], root_dir)
-#     p, r = precision_recall(patient_ids['engel2or3or4'], root_dir)
-#     precision.append(p)
-#     recall.append(r)
-# plt.figure()
-# plt.plot(recall, precision)
-# plt.xlabel('Recall', fontsize=13)
-# plt.ylabel('Precision', fontsize=13)
-# plt.title('Engel score II, III and IV', fontsize=15)
-# plt.show(block=False)
+    # precision = []
+    # recall = []
+    # nbins = 10
+    # onst_thrshld =  np.linspace(0, -0.5, 20)
+    # bin_thrshld = 1
+    # for onst_thrshld_ in onst_thrshld:
+    #     find_ez(onst_thrshld_, bin_thrshld, nbins, patient_ids['engel2or3or4'], root_dir)
+    #     p, r = precision_recall(patient_ids['engel2or3or4'], root_dir)
+    #     precision.append(p)
+    #     recall.append(r)
+    # plt.figure()
+    # plt.plot(recall, precision)
+    # plt.xlabel('Recall', fontsize=13)
+    # plt.ylabel('Precision', fontsize=13)
+    # plt.title('Engel score II, III and IV', fontsize=15)
+    # plt.show(block=False)
 
-# ## Bar plot
-# precision = []
-# recall = []
-# onst_thrshlds = [-0.05]
-# for threshold in onst_thrshlds:
-#     find_ez(threshold, patient_ids['engel1'], root_dir)
-#     p, r = precision_recall(patient_ids['engel1'], root_dir)
-#     precision.append(p)
-#     recall.append(r)
+    # ## Bar plot
+    # precision = []
+    # recall = []
+    # onst_thrshlds = [-0.05]
+    # for threshold in onst_thrshlds:
+    #     find_ez(threshold, patient_ids['engel1'], root_dir)
+    #     p, r = precision_recall(patient_ids['engel1'], root_dir)
+    #     precision.append(p)
+    #     recall.append(r)
 
-# ax = plt.subplot(111)
-# ax.bar([1,2], [precision[0],recall[0]], color=['black', 'grey'])
+    # ax = plt.subplot(111)
+    # ax.bar([1,2], [precision[0],recall[0]], color=['black', 'grey'])
 
-# precision = []
-# recall = []
-# onst_thrshlds = [-0.05]
-# for threshold in onst_thrshlds:
-#     find_ez(threshold, patient_ids['engel2or3or4'], root_dir)
-#     p, r = precision_recall(patient_ids['engel2or3or4'], root_dir)
-#     precision.append(p)
-#     recall.append(r)
+    # precision = []
+    # recall = []
+    # onst_thrshlds = [-0.05]
+    # for threshold in onst_thrshlds:
+    #     find_ez(threshold, patient_ids['engel2or3or4'], root_dir)
+    #     p, r = precision_recall(patient_ids['engel2or3or4'], root_dir)
+    #     precision.append(p)
+    #     recall.append(r)
 
-# ax.bar([5,6], [precision[0],recall[0]], color=['black', 'grey'])
-# ax.set_xticks([1.5, 5.5])
-# ax.set_xticklabels(['Engel score I', 'Engel score II, III or IV'], fontsize=15)
-# ax.tick_params(axis='y', labelsize=12)
-# ax.spines['top'].set_visible(False)
-# ax.spines['right'].set_visible(False)
-# legend_elements = [Line2D([0], [0], color='black', lw=5, label='Precision'),
-#                    Line2D([0], [0], color='grey', lw=5, label='Recall')]
-# ax.legend(handles=legend_elements)
-# plt.show()
+    # ax.bar([5,6], [precision[0],recall[0]], color=['black', 'grey'])
+    # ax.set_xticks([1.5, 5.5])
+    # ax.set_xticklabels(['Engel score I', 'Engel score II, III or IV'], fontsize=15)
+    # ax.tick_params(axis='y', labelsize=12)
+    # ax.spines['top'].set_visible(False)
+    # ax.spines['right'].set_visible(False)
+    # legend_elements = [Line2D([0], [0], color='black', lw=5, label='Precision'),
+    #                    Line2D([0], [0], color='grey', lw=5, label='Recall')]
+    # ax.legend(handles=legend_elements)
+    # plt.show()
 
 
-# tpr = []
-# fpr = []
-# onst_thrshld = -0.05
-# nbins = 50
-# bin_thrshld =  np.arange(1, nbins)
-# for bin_thrshld_ in bin_thrshld:
-#     find_ez(onst_thrshld, bin_thrshld_, nbins, patient_ids['engel1'], root_dir)
-#     tpr_, fpr_ = tpr_and_fpr(patient_ids['engel1'], root_dir)
-#     tpr.append(tpr_)
-#     fpr.append(fpr_)
-# plt.figure()
-# plt.plot(fpr, tpr)
-# plt.xlabel('False Positive Rate', fontsize=15)
-# plt.ylabel('True Positive Rate', fontsize=15)
-# plt.show()
+    # tpr = []
+    # fpr = []
+    # onst_thrshld = -0.05
+    # nbins = 50
+    # bin_thrshld =  np.arange(1, nbins)
+    # for bin_thrshld_ in bin_thrshld:
+    #     find_ez(onst_thrshld, bin_thrshld_, nbins, patient_ids['engel1'], root_dir)
+    #     tpr_, fpr_ = tpr_and_fpr(patient_ids['engel1'], root_dir)
+    #     tpr.append(tpr_)
+    #     fpr.append(fpr_)
+    # plt.figure()
+    # plt.plot(fpr, tpr)
+    # plt.xlabel('False Positive Rate', fontsize=15)
+    # plt.ylabel('True Positive Rate', fontsize=15)
+    # plt.show()
