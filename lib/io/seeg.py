62a3758c183219aa97036e9e40c455741d028d01
anirudh <anirudhnihalani@gmail.com>
2019-11-04 18:01:18 +0100

    Move data preprocessing code into lib submodules
    Preprocessing code to generate SLP was in retro_prepare_data.py
    module. Moved it to lib.preprocess module along and some IO functions
    are moved to lib.seeg

--- lib/io/seeg.py
+++ lib/io/seeg.py
@@ -1,5 +1,8 @@
 import numpy as np
 from .dmeeg import EEG
+import json
+import mne
+import os
 
 
 def _maybe_bip(seeg, time, proj_bip=None):
@@ -21,9 +24,114 @@ def load_eeg(fname, proj_bip=None):
     seeg = seeg[:-2]
     return _maybe_bip(seeg, time, proj_bip)
 
+
 def read_contacts(cntcts_file, type='dict'):
-    cntcts = zip(np.loadtxt(cntcts_file, usecols=[0], dtype='str'), np.loadtxt(cntcts_file, usecols=[1,2,3]))
-    if(type == 'dict'):
+    cntcts = zip(
+        np.loadtxt(cntcts_file, usecols=[0], dtype='str'),
+        np.loadtxt(cntcts_file, usecols=[1, 2, 3]))
+    if (type == 'dict'):
         return dict(cntcts)
-    elif(type == 'list'):
+    elif (type == 'list'):
         return list(cntcts)
+
+
+def read_seeg_xyz(data_dir):
+    lines = []
+    fname = os.path.join(data_dir, 'elec/seeg.xyz')
+    with open(fname, 'r') as fd:
+        for line in fd.readlines():
+            name, *sxyz = line.strip().split()
+            xyz = [float(_) for _ in sxyz]
+            lines.append((name, xyz))
+    return lines
+
+
+def read_gain(data_dir, picks):
+    gain = np.loadtxt(f'{data_dir}/elec/gain_inv-square.destrieux.txt')
+    seeg_xyz = read_seeg_xyz(data_dir)
+    # Re-order the gain matrix to match with order of channels read using MNE
+    gain_idxs = []
+    for label in picks:
+        for i, (gain_label, _) in enumerate(seeg_xyz):
+            if (label == gain_label):
+                gain_idxs.append(i)
+    gain = gain[gain_idxs]
+    return gain
+
+
+def find_picks(json_fnames):
+    picks = []
+    for fname in json_fnames:
+        fname_wo_xtnsn = os.path.splitext(os.path.basename(fname))[0]
+        with open(fname) as fd:
+            meta_data = json.load(fd)
+        if (meta_data['type'].lower() == "spontaneous seizure"):
+            exclude = meta_data['bad_channels'] + meta_data['non_seeg_channels']
+            ch_names = set(
+                mne.io.read_info(
+                    f'{os.path.dirname(fname)}/{fname_wo_xtnsn}.raw.fif')[
+                        'ch_names'])
+            picks.append(ch_names - set(exclude))
+        else:
+            raise Exception('Not a spontaneous seizure')
+    return set.intersection(*picks)
+
+
+def read_one_seeg(data_dir, meta_data_fname, raw_seeg_fname):
+    picks = find_picks(
+        [os.path.join(data_dir, 'seeg', 'fif', meta_data_fname)])
+    seeg = dict()
+    fname_wo_xtnsn = os.path.splitext(os.path.basename(raw_seeg_fname))[0]
+    with open(os.path.join(data_dir, 'seeg', 'fif', meta_data_fname)) as fd:
+        meta_data = json.load(fd)
+    if (meta_data['type'].lower() == "spontaneous seizure"):
+        raw = mne.io.Raw(
+            os.path.join(data_dir, 'seeg', 'fif', raw_seeg_fname),
+            verbose='WARNING',
+            preload=True)
+        assert meta_data['onset'] is not None and meta_data['termination'] is not None
+        raw.pick_types(meg=False, eeg=True)
+        raw.pick_channels(picks)
+        raw.reorder_channels(picks)
+        seeg['fname'] = f'{fname_wo_xtnsn}'
+        seeg['onset'] = meta_data['onset']
+        seeg['offset'] = meta_data['termination']
+        seeg['time_series'] = raw.get_data().T
+        seeg['sfreq'] = raw.info['sfreq']
+        seeg['picks'] = picks
+    return seeg
+
+
+def read_seeg(data_dir):
+    # Reads all seizures(.fif files) in the given folder
+    json_fnames = glob.glob(f'{data_dir}/seeg/fif/*.json')
+    json_fnames_woxtnsn = [fname.split('.json')[0] for fname in json_fnames]
+    fif_fnames = glob.glob(f'{data_dir}/seeg/fif/*.raw.fif')
+    fif_fnames_woxtnsn = [fname.split('.raw.fif')[0] for fname in fif_fnames]
+    json_fnames = list(
+        set.intersection(set(json_fnames_woxtnsn), set(fif_fnames_woxtnsn)))
+    json_fnames = [f'{el}.json' for el in json_fnames]
+    picks = find_picks(json_fnames)
+    data = {'seizures': [], 'picks': picks}
+    for fname in json_fnames:
+        t = {}
+        fname_wo_xtnsn = ''.join(fname.split('/')[-1].split('.json'))
+        with open(fname) as fd:
+            meta_data = json.load(fd)
+        if (meta_data['type'].lower() == "spontaneous seizure"):
+            raw = mne.io.Raw(
+                f'{data_dir}/seeg/fif/{fname_wo_xtnsn}.raw.fif',
+                verbose='WARNING',
+                preload=True)
+            assert meta_data['onset'] is not None and meta_data['termination'] is not None
+            #         raw.crop(tmin=meta_data['onset'], tmax=meta_data['termination'])
+            raw.pick_types(meg=False, eeg=True)
+            raw.pick_channels(picks)
+            raw.reorder_channels(picks)
+            t['fname'] = f'{fname_wo_xtnsn}'
+            t['onset'] = meta_data['onset']
+            t['offset'] = meta_data['termination']
+            t['seeg'] = raw.get_data().T
+            t['sfreq'] = raw.info['sfreq']
+            data['seizures'].append(t)
+    return data
--- lib/preprocess/envelope.py
+++ lib/preprocess/envelope.py
@@ -1,5 +1,12 @@
 from scipy import signal
 import numpy as np
+import lib.io.stan
+import lib.io.seeg
+import glob
+import os
+import zipfile
+import mne
+import json
 
 
 def bfilt(data, samp_rate, fs, mode, order=3, axis=-1):
@@ -22,18 +29,19 @@ def seeg_log_power(a, win_len, pad=True):
     # pad with zeros at the end to compute moving average of same length as the signal itself
     envlp_pad = np.pad(a, ((0, win_len), (0, 0)), 'constant')
     for i in range(a.shape[0]):
-        envlp[i, :] = np.log(np.mean(envlp_pad[i:i + win_len, :]**2, axis=0))
+        envlp[i, :] = np.log(
+            np.mean(envlp_pad[i:i + win_len, :]**2, axis=0) + 1)
     return envlp
 
 
-def compute_fitting_target(data,
-                           samp_rate,
-                           win_len=100,
-                           hpf=10.0,
-                           lpf=1.0,
-                           logtransform=False):
+def compute_slp_syn(data,
+                    samp_rate,
+                    win_len=100,
+                    hpf=10.0,
+                    lpf=1.0,
+                    logtransform=False):
     '''
-    Extracts smoothed log. power of given SEEG
+    Extracts smoothed log. power envelope of given SEEG for simulated data
     '''
     # high pass filter to remove baseline shift
     data_hpf = bfilt(data, samp_rate, hpf, 'highpass', axis=0)
@@ -43,3 +51,37 @@ def compute_fitting_target(data,
     # low pass filter the log power for smoothing
     data_lpwr = bfilt(data_lpwr, samp_rate, lpf, 'lowpass', axis=0)
     return data_lpwr
+
+
+def compute_slp(seeg, hpf=10.0, lpf=1.0, filter_order=5.0):
+    start_idx = int(seeg['onset'] * seeg['sfreq']) - int(seeg['sfreq'])
+    end_idx = int(seeg['offset'] * seeg['sfreq']) + int(seeg['sfreq'])
+    slp = seeg['time_series'][start_idx:end_idx]
+    # High pass filter the data
+    slp = bfilt(slp, seeg['sfreq'], hpf, 'highpass', axis=0)
+    # Compute seeg log power
+    slp = seeg_log_power(slp, 100)
+    # Low pass filter the data to smooth
+    slp = bfilt(slp, seeg['sfreq'], lpf, 'lowpass', axis=0)
+    return slp
+
+
+def prepare_data(data_dir, meta_data_fname, raw_seeg_fname, hpf=10.0, lpf=1.0):
+    try:
+        with zipfile.ZipFile(
+                f'{data_dir}/tvb/connectivity.destrieux.zip') as sczip:
+            with sczip.open('weights.txt') as weights:
+                SC = np.loadtxt(weights)
+                SC[np.diag_indices(SC.shape[0])] = 0
+                SC = SC / SC.max()
+    except FileNotFoundError as err:
+        print(f'Structural connectivity not found for {data_dir}')
+        return
+    # Read SEEG data from fif files
+    raw_data = lib.io.seeg.read_one_seeg(data_dir, meta_data_fname,
+                                         raw_seeg_fname)
+    gain = lib.io.seeg.read_gain(data_dir, raw_data['picks'])
+    # Compute seeg log power
+    slp = compute_slp(raw_data, hpf, lpf)
+    data = {'SC': SC, 'gain': gain, 'slp': slp}
+    return data
