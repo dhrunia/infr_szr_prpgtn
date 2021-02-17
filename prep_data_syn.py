bbe8e333b49cdec6be3a75225aa50c1c42aa877b
anirudhnihalani <anirudhnihalani@gmail.com>
2021-02-17 14:41:52 +0100

    Initial code to prepare data for inital conditions sweep for MAP

--- prep_data_syn.py
+++ prep_data_syn.py
@@ -87,3 +87,102 @@ fit_data = lib.io.stan.rload(os.path.join(results_dir,
                                           'fit_data_snsrfit_ode_snr4.5.R'))
 plt.figure(figsize=(20,5))
 plt.plot(fit_data['slp']);
+# %% [markdown]
+### Prepare data for different initial conditions
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+import lib.preprocess.envelope
+import os
+import lib.io.stan
+import scipy.stats as stats
+# %%
+data_dir = 'datasets/syn_data/id001_bt'
+results_dir = 'results/exp10/exp10.88.2'
+os.makedirs(results_dir, exist_ok=True)
+os.makedirs(f'{results_dir}/Rfiles',exist_ok=True)
+network = np.load(f'{data_dir}/network.npz')
+SC = network['SC']
+K = np.max(SC)
+SC = SC / K
+SC[np.diag_indices(SC.shape[0])] = 0
+
+gain_mat = network['gain_mat']
+
+slp = np.load(f'{data_dir}/fit_trgt.npz')['fit_trgt']
+npoints = 300
+ds_freq = int(np.round(slp.shape[0]/npoints))
+slp_ds = slp[0:-1:ds_freq,:]
+snsr_pwr = np.mean(slp_ds**2, axis=0)
+# %%
+nn = SC.shape[0]
+ns = gain_mat.shape[0]
+nt = slp_ds.shape[0]
+tvb_syn_data = np.load(os.path.join(
+    data_dir, 'syn_tvb_ez=48-79_pz=11-17-22-75.npz'))
+
+x0_mu = -3.0*np.ones(nn)
+ez_hyp = np.array(tvb_syn_data['ez'])
+pz_hyp = tvb_syn_data['pz']
+x0_mu[ez_hyp] = -1.8
+x0_mu[pz_hyp] = -2.3
+
+sigma_prior = np.arange(1.0, 11.0, 1.0)
+for el_sigma_prior in sigma_prior:
+    for i in range(100):
+        x0_clip_a, x0_clip_b = -5.0, 0.0
+        x0_loc = x0_mu
+        x0 = stats.truncnorm.rvs(a=(x0_clip_a - x0_loc)/el_sigma_prior, b=(
+            x0_clip_b - x0_loc)/el_sigma_prior, loc=x0_loc, scale=el_sigma_prior)
+
+        alpha_clip_a, alpha_clip_b = 0.0, 50.0
+        alpha_loc = 1.0
+        alpha = stats.truncnorm.rvs(a=(alpha_clip_a - alpha_loc)/el_sigma_prior, b=(
+            alpha_clip_b - alpha_loc)/el_sigma_prior, loc=alpha_loc, scale=el_sigma_prior)[0]
+
+        beta_clip_a, beta_clip_b = -10.0, 10.0
+        beta_loc = 0.0
+        beta = stats.truncnorm.rvs(a=(beta_clip_a - beta_loc)/el_sigma_prior, b=(
+            beta_clip_b - beta_loc)/el_sigma_prior, loc=beta_loc, scale=el_sigma_prior)[0]
+
+        K_clip_a, K_clip_b = 0.0, 10.0
+        K_loc = 1.0
+        K = stats.truncnorm.rvs(a=(K_clip_a - K_loc)/el_sigma_prior, b=(
+            K_clip_b - K_loc)/el_sigma_prior, loc=K_loc, scale=el_sigma_prior)[0]
+
+        tau0_clip_a, tau0_clip_b = 10.0, 100.0
+        tau0_loc = 20.0
+        tau0 = stats.truncnorm.rvs(a=(tau0_clip_a - tau0_loc)/el_sigma_prior, b=(
+            tau0_clip_b - tau0_loc)/el_sigma_prior, loc=tau0_loc, scale=el_sigma_prior)[0]
+
+        eps_slp_clip_a, eps_slp_clip_b = 0.5, 100.0
+        eps_slp_loc = 1.0
+        eps_slp = stats.truncnorm.rvs(a=(eps_slp_clip_a - eps_slp_loc)/el_sigma_prior, b=(
+            eps_slp_clip_b - eps_slp_loc)/el_sigma_prior, loc=eps_slp_loc, scale=el_sigma_prior)[0]
+
+        eps_snsr_pwr_clip_a, eps_snsr_pwr_clip_b = 0.5, 100.0
+        eps_snsr_pwr_loc = 1.0
+        eps_snsr_pwr = stats.truncnorm.rvs(a=(eps_snsr_pwr_clip_a - eps_snsr_pwr_loc)/el_sigma_prior, b=(
+            eps_snsr_pwr_clip_b - eps_snsr_pwr_loc)/el_sigma_prior, loc=eps_snsr_pwr_loc, scale=el_sigma_prior)[0]
+
+        x_init_clip_a, x_init_clip_b = -10.0, -1.0
+        x_init_loc = -2.0*np.ones(nn)
+        x_init = stats.truncnorm.rvs(a=(x_init_clip_a - x_init_loc)/el_sigma_prior, b=(
+            x_init_clip_b - x_init_loc)/el_sigma_prior, loc=x_init_loc, scale=el_sigma_prior)
+
+        z_init_clip_a, z_init_clip_b = 2.0, 10.0
+        z_init_loc = 3.5*np.ones(nn)
+        z_init = stats.truncnorm.rvs(a=(z_init_clip_a - z_init_loc)/el_sigma_prior, b=(
+            z_init_clip_b - z_init_loc)/el_sigma_prior, loc=z_init_loc, scale=el_sigma_prior)
+
+        param_init = {'x0': x0, 'alpha': alpha,
+                      'beta': beta, 'K': K, 'tau0': tau0, 'x_init': x_init, 'z_init': z_init,
+                      'eps_slp': eps_slp, 'eps_snsr_pwr': eps_snsr_pwr}
+        lib.io.stan.rdump(
+            f'{results_dir}/Rfiles/param_init_sigmaprior{el_sigma_prior:.0f}_sample{i}.R', param_init)
+
+data = {'nn': nn, 'ns': ns, 'nt': nt, 'SC': SC, 'gain': gain_mat,
+        'slp': slp_ds, 'snsr_pwr': snsr_pwr, 'x0_mu': x0_mu}
+input_Rfile = f'fit_data_snsrfit_ode.R'
+os.makedirs(f'{results_dir}/Rfiles', exist_ok=True)
+lib.io.stan.rdump(f'{results_dir}/Rfiles/{input_Rfile}', data)
