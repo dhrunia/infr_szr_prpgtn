a6b1ecf231c8399c137edde32cd3128b8551e69a
dhrunia <dhrunia@mailbox.org>
2022-01-10 22:59:12 +0530

    explicitly cast int variables and use tf instead of numpy

--- lib/utils/sht.py
+++ lib/utils/sht.py
@@ -21,16 +21,17 @@ def prep(L_MAX):
 
 
 def analys(N_LON, F_theta_phi, glq_wts, P_l_m_costheta):
+    pi = tf.constant(np.math.pi, dtype=tf.complex64)
     F_theta_m = tf.signal.rfft(F_theta_phi)
-    F_theta_m_glq_wtd = F_theta_m * glq_wts[:, np.newaxis]
+    F_theta_m_glq_wtd = F_theta_m * glq_wts[:, tf.newaxis]
     F_l_m = tf.einsum('ijk,kj->ij', P_l_m_costheta,
-                      F_theta_m_glq_wtd) * (2*np.math.pi/N_LON)
+                      F_theta_m_glq_wtd) * (2*pi/tf.cast(N_LON, tf.complex64))
     return F_l_m
 
 
 def synth(N_LON, F_l_m, P_l_m_costheta):
     F_theta_m = tf.einsum('ij,ijk->kj', F_l_m, P_l_m_costheta)
-    F_theta_phi = N_LON * \
+    F_theta_phi = tf.cast(N_LON, tf.float32) * \
         tf.signal.irfft(F_theta_m, fft_length=tf.constant(
             N_LON, dtype=tf.int32, shape=[1]))
     return F_theta_phi
