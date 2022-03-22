7fea1c42bb626011e65c26eb8471be091e2e68ac
dhrunia <dhrunia@mailbox.org>
2022-03-22 10:41:20 +0100

    system maintenance

--- 2depileptor_neural_field.py
+++ 2depileptor_neural_field.py
@@ -13,20 +13,30 @@ D = tf.constant(-0.01, dtype=tf.float32)
 l = tf.range(0, L_MAX+1, dtype=tf.float32)
 Dll = tf.cast(D * l * (l+1), dtype=tf.complex64)
 # %%
+
+
 def epileptor2D_nf_ode_fn(t, y, x0, tau):
     nn = N_LAT * N_LON
     x = y[0:nn]
     z = y[nn:2*nn]
     I1 = tf.constant(4.1, dtype=tf.float32)
-    x_lm = tfsht.analys(N_LON, tf.reshape(tf.math.sigmoid(x), (N_LAT, N_LON)), 
-                        glq_wts, P_l_m_costheta)
+    # NOTE: alpha > 7.0 is causing DormandPrince integrator to diverge
+    alpha = tf.constant(5.0, dtype=tf.float32)
+    theta = tf.constant(-1.0, dtype=tf.float32)
+    gamma = tf.constant(1.0, dtype=tf.float32)
+    x_lm = tfsht.analys(N_LON, tf.reshape(tf.math.sigmoid(
+        alpha*(x - theta)), (N_LAT, N_LON)), glq_wts, P_l_m_costheta)
     x_lm_hat = Dll[:, tf.newaxis] * x_lm
-    local_cplng = tf.reshape(tfsht.synth(N_LON, x_lm_hat, P_l_m_costheta), [-1])
-    dx = 1.0 - tf.math.pow(x, 3) - 2 * tf.math.pow(x, 2) - z + I1 + local_cplng
+    local_cplng = tf.reshape(tfsht.synth(
+        N_LON, x_lm_hat, P_l_m_costheta), [-1])
+    dx = 1.0 - tf.math.pow(x, 3) - 2 * tf.math.pow(x, 2) - \
+        z + I1 + gamma * local_cplng
     # gx = tf.reduce_sum(K * SC * (x[tf.newaxis, :] - x[:, tf.newaxis]), axis=1)
     dz = (1.0/tau)*(4*(x - x0) - z)
     return tf.concat((dx, dz), axis=0)
 # %%
+
+
 def euler_integrator(ode_fn, nsteps, sampling_period, time_step, y_init, x0, tau):
     y = tf.TensorArray(dtype=tf.float32, size=nsteps, clear_after_read=False)
     y_next = y_init
@@ -35,6 +45,7 @@ def euler_integrator(ode_fn, nsteps, sampling_period, time_step, y_init, x0, tau
             y_next = y_next + time_step * ode_fn(0.0, y_next, x0, tau)
         y = y.write(i, y_next)
     return y.stack()
+
 
 def rk4_integrator(ode_fn, nsteps, sampling_period, time_step, y_init, x0, tau):
     y = tf.TensorArray(dtype=tf.float32, size=nsteps, clear_after_read=False)
@@ -50,16 +61,17 @@ def rk4_integrator(ode_fn, nsteps, sampling_period, time_step, y_init, x0, tau):
         y = y.write(i, y_next)
     return y.stack()
 
+
 def bdf_integrator(ode_fn, t_init, y_init, solution_times, constants):
     return tfp.math.ode.BDF().solve(ode_fn=ode_fn,
-                             initial_time=t_init,
-                             initial_state=y_init,
-                             solution_times=solution_times,
-                             constants=constants)
+                                    initial_time=t_init,
+                                    initial_state=y_init,
+                                    solution_times=solution_times,
+                                    constants=constants)
 
 
 def dormandprince_integrator(ode_fn, t_init, y_init, solution_times, constants):
-    return tfp.math.ode.DormandPrince(atol=1e-2, 
+    return tfp.math.ode.DormandPrince(atol=1e-6,
                                       rtol=1e-3,
                                       validate_args=True).solve(
                                           ode_fn=ode_fn,
@@ -68,13 +80,18 @@ def dormandprince_integrator(ode_fn, t_init, y_init, solution_times, constants):
                                           solution_times=solution_times,
                                           constants=constants)
 # %%
+
+
 t_init = tf.constant(0.0, dtype=tf.float32)
-x_init_true = tf.constant(-2.0, dtype=tf.float32) * tf.ones(N_LAT*N_LON, dtype=tf.float32)
-z_init_true = tf.constant(5.0, dtype=tf.float32) * tf.ones(N_LAT*N_LON, dtype=tf.float32)
+x_init_true = tf.constant(-2.0, dtype=tf.float32) * \
+    tf.ones(N_LAT*N_LON, dtype=tf.float32)
+z_init_true = tf.constant(5.0, dtype=tf.float32) * \
+    tf.ones(N_LAT*N_LON, dtype=tf.float32)
 y_init_true = tf.concat((x_init_true, z_init_true), axis=0)
 tau_true = tf.constant(25, dtype=tf.float32)
 # x0_true = tf.constant(tvb_syn_data['x0'], dtype=tf.float32)
-x0_true = tf.constant(-1.8, dtype=tf.float32) * tf.ones(N_LAT*N_LON, dtype=tf.float32)
+x0_true = tf.constant(-1.8, dtype=tf.float32) * \
+    tf.ones(N_LAT*N_LON, dtype=tf.float32)
 t_init = tf.constant(0.0, dtype=tf.float32)
 
 # %%
@@ -82,14 +99,14 @@ nsteps = 300
 sampling_period = 0.1
 time_step = 0.01
 start_time = time.time()
-y_true = euler_integrator(epileptor2D_nf_ode_fn, nsteps, sampling_period, 
+y_true = euler_integrator(epileptor2D_nf_ode_fn, nsteps, sampling_period,
                           time_step, y_init_true, x0_true, tau_true)
 print(f"Time elapsed: {time.time() - start_time} seconds")
 
 x_true = y_true[:, :N_LAT*N_LON]
 z_true = y_true[:, N_LAT*N_LON:]
 idx = 10
-plt.figure(figsize=(7,5), dpi=150, constrained_layout=True)
+plt.figure(figsize=(7, 5), dpi=150, constrained_layout=True)
 plt.subplot(211)
 plt.ylabel(r'$x$', fontsize=15)
 plt.plot(x_true[:, idx])
@@ -99,7 +116,7 @@ plt.ylabel(r'$z$', fontsize=15)
 plt.suptitle(f"Euler Integration - dt={time_step}")
 
 # %%
-nsteps = 300
+nsteps = 600
 sampling_period = 0.1
 time_step = 0.1
 start_time = time.time()
@@ -110,7 +127,7 @@ print(f"Time elapsed: {time.time() - start_time} seconds")
 x_true = y_true[:, :N_LAT*N_LON]
 z_true = y_true[:, N_LAT*N_LON:]
 idx = 10
-plt.figure(figsize=(7,5), dpi=150, constrained_layout=True)
+plt.figure(figsize=(7, 5), dpi=150, constrained_layout=True)
 plt.subplot(211)
 plt.ylabel(r'$x$', fontsize=15)
 plt.plot(x_true[:, idx], color='black', alpha=0.1)
@@ -122,14 +139,14 @@ plt.suptitle(f"RK4 Integration - dt={time_step}")
 time_step = tf.constant(0.1, dtype=tf.float32)
 solution_times = time_step * tf.range(0, 300, dtype=tf.float32)
 start_time = time.time()
-y_true = dormandprince_integrator(epileptor2D_nf_ode_fn, t_init, y_init_true, 
-                        solution_times, {'x0':x0_true, 'tau':tau_true})
+y_true = dormandprince_integrator(epileptor2D_nf_ode_fn, t_init, y_init_true,
+                                  solution_times, {'x0': x0_true, 'tau': tau_true})
 print(f"Time elapsed: {time.time() - start_time} seconds")
 
 x_true = y_true.states[:, :N_LAT*N_LON]
 z_true = y_true.states[:, N_LAT*N_LON:]
 idx = 10
-plt.figure(figsize=(7,5), dpi=150, constrained_layout=True)
+plt.figure(figsize=(7, 5), dpi=150, constrained_layout=True)
 plt.subplot(211)
 plt.ylabel(r'$x$', fontsize=15)
 plt.plot(x_true[:, idx])
